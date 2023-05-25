c9dac154be0694fc7f64235ae4f0e38b3926d37c
BrunaSilva <bruna-nascimento_@hotmail.com>
2023-05-25 18:22:43 -0300

    Alterações requisito 1

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,5 +1,3 @@
-import { createCityElement } from "./pageFunctions";
-
 const TOKEN = import.meta.env.VITE_TOKEN;
 
 export const searchCities = (term) => {
@@ -12,15 +10,13 @@ export const searchCities = (term) => {
   fetch(`http://api.weatherapi.com/v1/search.json?${query.toString()}`)
     .then((response) => response.json())
     .then((data) => {
-      return data;
+      if (!data.length) return window.alert('Nenhuma cidade encontrada');
     })
     .catch((error) => {
-      if (!data.length) {
-        return window.alert('Nenhuma cidade encontrada');
-      }
+      return error.message;
     });
 };
 
-export const getWeatherByCity = (cityURL) => {
+export const getWeatherByCity = () => {
 
 };
