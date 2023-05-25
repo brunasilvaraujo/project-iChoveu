9e020c806221645fb0cdeb2f73a84958dd531809
BrunaSilva <bruna-nascimento_@hotmail.com>
2023-05-25 18:14:38 -0300

    Requisito 1

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,9 +1,26 @@
-// Remova os comentários a medida que for implementando as funções
+import { createCityElement } from "./pageFunctions";
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+const TOKEN = import.meta.env.VITE_TOKEN;
+
+export const searchCities = (term) => {
+  const query = new URLSearchParams();
+
+  query.append('lang', 'pt');
+  query.append('key', TOKEN);
+  query.append('q', term);
+
+  fetch(`http://api.weatherapi.com/v1/search.json?${query.toString()}`)
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      if (!data.length) {
+        return window.alert('Nenhuma cidade encontrada');
+      }
+    });
 };
 
-export const getWeatherByCity = (/* cityURL */) => {
-//   seu código aqui
+export const getWeatherByCity = (cityURL) => {
+
 };
